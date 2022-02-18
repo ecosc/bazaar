7d4c56498c793d02bc2fe6c7da7629d230e9fcc6
ecosc <info@ecosc.io>
2022-02-18 19:52:15 +0330

    add fee to profile create transaction

--- migrations/2_profile_contract_migration.js
+++ migrations/2_profile_contract_migration.js
@@ -1,5 +1,7 @@
 const ProfileContract = artifacts.require("Profile");
 
+const ECU_TOKEN_ADDRESS = '0xB7eb742aa5ddc7f08e5bB94E238D738126239F93';
+
 module.exports = function (deployer) {
-  deployer.deploy(ProfileContract);
+  deployer.deploy(ProfileContract, ECU_TOKEN_ADDRESS);
 };
